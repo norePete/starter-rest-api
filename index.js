a5643f25f272aef1ef3bafa5f13f0d41fb4292be
pete <peter.hooper@halter.co.nz>
2023-08-21 12:07:58 +1200

    wip

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 const db = require('@cyclic.sh/dynamodb')
 
 app.use(express.json())
@@ -25,8 +26,33 @@ app.use(express.static('beam', options))
 app.get('/', function(req, res) {
     res.send('root');
 });
-app.get('/fish', function(req, res) {
-    res.send('Salmon');
+app.get('/rotate', function(req, res) {
+
+    fs.readFile('beam/css/dynamic.css', 'utf8')
+    .then(data => {
+        const hexCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+        function getRandomCharacter() {
+            const randomIndex = Math.floor(Math.random() * hexCharacters.length);
+            return hexCharacters[randomIndex];
+        }
+        function generateRandomHexColor() {
+            let hexColor = '#';
+            for (let i = 0; i < 6; i++) {
+                hexColor += getRandomCharacter();
+            }
+            return hexColor;
+        }
+        random_color = generateRandomHexColor()
+        const hex = /#[A-Fa-f0-9]{6}\b/g;
+        const modifiedData = data.replace(hex, random_color);
+        return fs.writeFile('beam/css/dynamic.css', modifiedData, 'utf8');
+    })
+    .then(() => {
+    })
+    .catch(err => {
+        console.error(err);
+    });
+    res.send('success');
 });
 // Create or Update an item
 app.post('/:col/:key', async (req, res) => {
